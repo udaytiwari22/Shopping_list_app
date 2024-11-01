0aac5a6f879eb03b3e8388029f39af426035e0bf
Uday Tiwari <udaytiwaricyef2001@gmail.com>
2024-11-01 19:38:37 +0530

    updated

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -25,19 +25,17 @@ class _NewItemState extends State<NewItem> {
       _formkey.currentState!.save();
       final url = Uri.https('flutter-prep-56341-default-rtdb.firebaseio.com',
           'shopping-list.json');
-      final response = await http.post(
-        url,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: json.encode(
-          {
-            'name': _enteredName,
-            'quantity': _enteredQuantity,
-            'category': _selectedCategory.title,
+      final response = await http.post(url,
+          headers: {
+            'Content-Type': 'application/json',
           },
-        ),
-      );
+          body: json.encode(
+            {
+              'name': _enteredName,
+              'quantity': _enteredQuantity,
+              'category': _selectedCategory.title,
+            },
+          ));
 
       final Map<String, dynamic> resData = json.decode(response.body);
       if (!context.mounted) {
